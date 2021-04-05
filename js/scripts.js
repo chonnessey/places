374dbecc165195c7f5da8a024a6e753231def6f6
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-05 10:47:06 -0700

    add initial scripts language
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function Place() {
+  this.name = name;
+  this.location = location;
+  this.landmarks = landmarks;
+  this.season = season;
+  this.fact = fact;
+}
+
+let place1 = new Place("Milan", "Italy", ["The Duomo", "San Siro", "La Scala"], "Winter", "Has the most skyscrapers in Italy!")
