14a2f0b77e0598c0e5b852ede1e12d344e843249
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-05 13:27:13 -0700

    add UI logic to display results in HTML
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,11 @@ function Place(name, location, landmarks, season, fact) {
   this.fact = fact;
 }
 
-let place1 = new Place("Milan", "Italy", ["The Duomo", "San Siro", "La Scala"], "Winter", "Has the most skyscrapers in Italy!");
+// let place1 = new Place("Milan", "Italy", ["The Duomo", "San Siro", "La Scala"], "Winter", "Has the most skyscrapers in Italy!");
 
-let place2 = new Place("Sinaloa", "Mexico", ["Adrian's gma's house", "Malecon"], "Summer", "Mazatlan has the longest Boulevard in Mexico.");
+// let place2 = new Place("Sinaloa", "Mexico", ["Adrian's gma's house", "Malecon"], "Summer", "Mazatlan has the longest Boulevard in Mexico.");
 
-let place3 = new Place("Squamish", "Canada", ["The Chief", "Brohm Lake"], "Summer", "Squamish has some of the best climbing in North America");
+// let place3 = new Place("Squamish", "Canada", ["The Chief", "Brohm Lake"], "Summer", "Squamish has some of the best climbing in North America");
 
 
 
@@ -27,6 +27,8 @@ $(document).ready(function() {
     const inputSeason = $("#season").val();
     const inputFact = $("#fact").val();
     let newPlace = new Place(inputName, inputLocation, inputLandmarks, inputSeason, inputFact);
-    
-  })
-})
+console.log(newPlace);
+    // $(".new-place").append("<div>" + "<ul>" + "<li>" + inputName + "<li>" + inputLocation + "<li>" + inputLandmarks + "<li>" + inputSeason + "<li>" + inputFact)
+    $(".new-place").append("<p>" + Object.values(newPlace));
+  });
+});
