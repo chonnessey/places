d740068e5dc659d582a81a4b5a4c1adc12020b8e
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-05 10:57:58 -0700

    add all places to scripts page
    Co-authored-by: Thomas Friedrichs <friedrichs.tommy@gmail.com>
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,8 @@ function Place() {
   this.fact = fact;
 }
 
-let place1 = new Place("Milan", "Italy", ["The Duomo", "San Siro", "La Scala"], "Winter", "Has the most skyscrapers in Italy!")
+let place1 = new Place("Milan", "Italy", ["The Duomo", "San Siro", "La Scala"], "Winter", "Has the most skyscrapers in Italy!");
+
+let place2 = new Place("Sinaloa", "Mexico", ["Adrian's gma's house", "Malecon"], "Summer", "Mazatlan has the longest Boulevard in Mexico.");
+
+let place3 = new Place("Squamish", "Canada", ["The Chief", "Brohm Lake"], "Summer", "Squamish has some of the best climbing in North America");
